78ab107a20e010a04c2a9eb620fb1f212d6837c1
yjinlee99 <yjinlee99@naver.com>
2023-12-28 01:16:49 +0900

    [add] userController2

--- BookRealm_Project/src/user/UserController2.java
+++ BookRealm_Project/src/user/UserController2.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class UserController {
+public class UserController2 {
 
     Scanner sc = new Scanner(System.in);
 
