57e7e3aacb0c705008a135161bbf90dd43e6f383
dana096 <carpediem096@naver.com>
2023-12-29 11:15:46 +0900

    book-vo, dao  수정

--- BookRealm_Project/src/book/BookDAO.java
+++ BookRealm_Project/src/book/BookDAO.java
@@ -4,6 +4,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import book.BookVO;
 import ConnUtil.CloseHelper;
 import ConnUtil.ConnectionSingletonHelper;
 
@@ -221,5 +222,212 @@ public class BookDAO {
 
 		return bookId;
 	}
+	
+	// 도서 베스트셀러(5개) 조회
+		public List<BookVO> bestseller() throws SQLException {
+			ArrayList<BookVO> list = new ArrayList<>();
+			BookVO vo = null;
+
+			try {
+				// 작업 객체 생성
+				stmt = conn.createStatement();
+
+				// 쿼리문 준비 - select
+				String sql = "SELECT BOOKID, CATEGORY, TITLE, WRITER FROM BOOK ORDER BY SALES DESC LIMIT 5";
+
+				// 실행 및 리턴
+				rs = stmt.executeQuery(sql);
+
+				ResultSetMetaData rsmd = rs.getMetaData();
+				int count = rsmd.getColumnCount(); // db 테이블의 필드 갯수
+
+				while (rs.next()) {
+
+					vo = new BookVO();
+
+					vo.setBookId(rs.getInt("bookId"));
+					vo.setCategory(rs.getString("category"));
+					vo.setWriter(rs.getString("writer"));
+					vo.setTitle(rs.getString("title"));
+
+					list.add(vo);
+
+				} // while end
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				// 닫기 (자원 반환)
+//				CloseHelper.close(rs);
+//				CloseHelper.close(stmt);
+//				CloseHelper.close(conn);
+			}
+			return list;
+		}
+
+		// 카테고리로 도서 조회
+		public List<BookVO> selectBycategory(String category) throws SQLException {
+			ArrayList<BookVO> list = new ArrayList<>();
+			BookVO vo = null;
+
+			// 쿼리문 준비 - select
+			String sql = "SELECT * FROM BOOK WHERE category = ?";
+
+			try {
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setString(1, category);
+				rs = pstmt.executeQuery();
+
+				ResultSetMetaData rsmd = rs.getMetaData();
+				int count = rsmd.getColumnCount(); // db 테이블의 필드 갯수
+
+				while (rs.next()) {
+					
+					vo = new BookVO();
+
+					vo.setBookId(rs.getInt("bookId"));
+					vo.setCategory(rs.getString("category"));
+					vo.setWriter(rs.getString("writer"));
+					vo.setTitle(rs.getString("title"));
+					
+					list.add(vo);
+
+				} // while end
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				// 닫기 (자원 반환)
+//				CloseHelper.close(rs);
+//				CloseHelper.close(stmt);
+//				CloseHelper.close(conn);
+			}
+			return list;
+		}
+
+		// 제목으로 도서 조회
+		public List<BookVO> selectBytitle(String title) throws SQLException {
+			ArrayList<BookVO> list = new ArrayList<>();
+			BookVO vo = null;
+
+			// 쿼리문 준비 - select
+			String sql = "SELECT * FROM BOOK WHERE title = ?";
+
+			try {
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setString(1, title);
+				rs = pstmt.executeQuery();
+
+				ResultSetMetaData rsmd = rs.getMetaData();
+				int count = rsmd.getColumnCount(); // db 테이블의 필드 갯수
+
+				while (rs.next()) {
+
+					vo = new BookVO();
+
+					vo.setBookId(rs.getInt("bookId"));
+					vo.setCategory(rs.getString("category"));
+					vo.setWriter(rs.getString("writer"));
+					vo.setTitle(rs.getString("title"));
+					
+					list.add(vo);
+
+				} // while end
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				// 닫기 (자원 반환)
+//				CloseHelper.close(rs);
+//				CloseHelper.close(stmt);
+//				CloseHelper.close(conn);
+			}
+			return list;
+		}
+
+		// 작가로 도서 조회
+		public List<BookVO> selectBywriter(String writer) throws SQLException {
+			ArrayList<BookVO> list = new ArrayList<>();
+			BookVO vo = null;
+
+			// 쿼리문 준비 - select
+			String sql = "SELECT * FROM BOOK WHERE writer = ?";
+
+			try {
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setString(1, writer);
+				rs = pstmt.executeQuery();
+
+				ResultSetMetaData rsmd = rs.getMetaData();
+				int count = rsmd.getColumnCount(); // db 테이블의 필드 갯수
+
+				while (rs.next()) {
+
+					vo = new BookVO();
+
+					vo.setBookId(rs.getInt("bookId"));
+					vo.setCategory(rs.getString("category"));
+					vo.setWriter(rs.getString("writer"));
+					vo.setTitle(rs.getString("title"));
+					
+					list.add(vo);
+
+				} // while end
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				// 닫기 (자원 반환)
+//				CloseHelper.close(rs);
+//				CloseHelper.close(stmt);
+//				CloseHelper.close(conn);
+			}
+			return list;
+		}
+		
+		// 도서 상세보기
+		public List<BookVO> viewDetails(int bookId) throws SQLException {
+			ArrayList<BookVO> list = new ArrayList<>();
+			BookVO vo = null;
+
+			// 쿼리문 준비 - select
+			String sql = "SELECT * FROM BOOK WHERE bookId = ?";
+
+			try {
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1, bookId);
+				rs = pstmt.executeQuery();
+
+				ResultSetMetaData rsmd = rs.getMetaData();
+				int count = rsmd.getColumnCount(); // db 테이블의 필드 갯수
+
+				while (rs.next()) {
+
+					vo = new BookVO();
+
+					vo.setBookId(rs.getInt("bookId"));
+					vo.setCategory(rs.getString("category"));
+					vo.setWriter(rs.getString("writer"));
+					vo.setTitle(rs.getString("title"));
+					vo.setPrice(rs.getInt("price"));
+					vo.setStock(rs.getInt("stock"));
+					vo.setSales(rs.getInt("sales"));
+					vo.setPublishDate(rs.getDate("publishDate"));
+					vo.setPublisher(rs.getString("publisher"));
+					
+					list.add(vo);
+
+				} // while end
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				// 닫기 (자원 반환)
+//				CloseHelper.close(rs);
+//				CloseHelper.close(stmt);
+//				CloseHelper.close(conn);
+			}
+			return list;
+		}
 
 }
