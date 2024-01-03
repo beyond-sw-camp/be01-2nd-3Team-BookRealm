ba1e6bb3609197ff050bcb608f5c8e638931dd11
choi <jacobis@naver.com>
2024-01-03 23:08:25 +0900

    [ADD] 01.03 Orderlist, Review DTO 생성

---
+++ src/test/java/com/bookrealm/BookrealmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookrealm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookrealmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
