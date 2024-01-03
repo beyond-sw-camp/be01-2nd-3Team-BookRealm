4b6faf30391e4a0c137e0b5643b23d245a69f1f4
yjinlee99 <yjinlee99@naver.com>
2024-01-03 23:50:19 +0900

    진행중..

---
+++ bookrealm/src/main/java/com/bookrealm/Model/OrderList.java
@@ -0,0 +1,16 @@
+package com.bookrealm.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class OrderList {
+    @Id
+    @GeneratedValue
+    private Long id;
+}
