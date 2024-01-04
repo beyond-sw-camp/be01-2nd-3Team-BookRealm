c0f935302c2811a5c5c3e5e3790845a76e7cee17
yjinlee99 <yjinlee99@naver.com>
2024-01-04 17:20:10 +0900

    [update] 폴더 정리

--- bookrealm/src/main/java/com/bookrealm/repository/BookRepository.java
+++ bookrealm/src/main/java/com/bookrealm/repository/BookRepository.java
@@ -1,11 +1,11 @@
-package com.bookrealm.book.repository;
+package com.bookrealm.repository;
 
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.bookrealm.book.model.Book;
+import com.bookrealm.model.Book;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
