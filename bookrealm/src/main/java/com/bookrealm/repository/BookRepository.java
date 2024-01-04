d1d4fe49010eb0feb19eff48effda3e7bf446384
dana096 <carpediem096@naver.com>
2024-01-04 16:41:04 +0900

    duplication class

--- bookrealm/src/main/java/com/bookrealm/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookrealm.repository;
-
-import com.bookrealm.model.book;
-@Repository
-public interface bookRepository extends JpaRepository<book, Integer> {
-
-    Optional<book> findByISBN(Integer ISBN);
-
-}
