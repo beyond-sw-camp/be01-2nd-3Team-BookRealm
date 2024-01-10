df776a527f8ac8eef41c8ff96b5db7b80061aeb6
dana096 <carpediem096@naver.com>
2024-01-10 11:40:53 +0900

    [add] Cart 관련 파일 업로드

--- bookrealm/src/main/java/com/bookrealm/model/Cart.java
+++ bookrealm/src/main/java/com/bookrealm/model/Cart.java
@@ -1,29 +1,54 @@
 package com.bookrealm.model;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import static jakarta.persistence.FetchType.LAZY;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
 
 @Data
 @Entity
 public class Cart {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "cart_id")
-    private Long id;  // 장바구니 코드 (PK)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "cart_id")
+	private Long id; // 장바구니 코드 (PK)
 
-    @Column(nullable = false)
-    private Integer purchase;  //수량
+	@Column(nullable = false, columnDefinition = "INT DEFAULT 1")
+	private Integer purchase; // 수량
 
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+	@ManyToOne(fetch = LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
 
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "book_id")
-    private Book book;
+	@ManyToOne(fetch = LAZY)
+	@JoinColumn(name = "book_id")
+	private Book book;
+
+	// user가 처음 장바구니에 책을 담기위해 장바구니 생성
+	public static Cart createCart(User user) {
+		Cart cart = new Cart();
+		cart.setPurchase(0);
+		cart.setUser(user);
+		return cart;
+	}
+
+	// 장바구니에 담을 책 수량 증가시켜주는 메소드
+	public static Cart createCartBook(User user, Book book, int purchase) {
+		Cart cartBook = new Cart();
+		cartBook.setUser(user);
+		cartBook.setPurchase(purchase);
+		cartBook.setBook(book);
+		return cartBook;
+	}
+
+	public void addPurchase(int purchase) {
+		this.purchase += purchase;
+	}
 }
---
+++ bookrealm/src/main/java/com/bookrealm/service/CartService.java
@@ -0,0 +1,67 @@
+package com.bookrealm.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.bookrealm.model.Book;
+import com.bookrealm.model.Cart;
+import com.bookrealm.model.User;
+import com.bookrealm.repository.BookRepository;
+import com.bookrealm.repository.CartRepository;
+import com.bookrealm.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CartService {
+	
+	private final BookRepository bookRepository;
+	private final UserRepository userRepository;
+	private final CartRepository cartRepository;
+
+	// User에게 장바구니 생성
+	public void createCart(User user) {
+		Cart cart = Cart.createCart(user);
+		cartRepository.save(cart);
+	}
+	
+	// 장바구니에 book 추가
+	@Transactional
+	public Cart addCart(User user, Book book, int purchase) {
+		
+		
+		Cart cart = cartRepository.findByIdAndBookId(user.getId(), book.getId());
+		
+		// 상품이 장바구니에 존재하지 않는다면 카트상품 생성
+		if (cart == null) {
+			cart = Cart.createCartBook(user, book, purchase);
+			cartRepository.save(cart);
+		}
+		
+		// 상품이 장바구니에 이미 존재한다면 수량만 증가
+		else {
+			cart.addPurchase(purchase);
+		}
+		return cartRepository.save(cart);
+	}
+	
+	// 장바구니 조회하기
+	public List<Cart> userCartView(Cart cart) {
+		List<Cart> cart_books = cartRepository.findAll();
+		List<Cart> user_books = new ArrayList<>();
+		
+		for (Cart cart_book : cart_books) {
+            if (cart_book.getId() == cart.getId()) {
+            	cart_books.add(cart_book);
+            }
+        }
+
+        return user_books;
+	}
+
+}
