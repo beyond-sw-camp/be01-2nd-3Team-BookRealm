3517f4bb2530d21424e7d85ac34fc8fbf8861611
dana096 <carpediem096@naver.com>
2024-01-16 14:49:34 +0900

    불필요한 코드 정리중..

--- bookrealm/src/main/java/com/bookrealm/controller/BookController.java
+++ bookrealm/src/main/java/com/bookrealm/controller/BookController.java
@@ -35,28 +35,6 @@ public class BookController {
         this.memberService = memberService;
     }
 
-//    한 페이지에 리스트 출력
-//    @GetMapping("/books")
-//    public String findAll(Model model) {
-//        List<Book> allBooks = bookService.findAll();
-//        model.addAttribute("allBooks", allBooks);
-//        return "index";  
-//    }
-
-    @GetMapping("/all")
-    public String getAll(Model model, @RequestParam(name = "page", defaultValue = "0") int page,
-                         @RequestParam(name = "size", defaultValue = "20") int size) {  // page : 현재 페이지 번호, size : 한 페이지에 보여질 항목 수
-        // 페이징 정보를 생성하여 BookService의 findAll 메서드에 전달
-        PageRequest pageable = PageRequest.of(page, size);
-        // BookService의 findAll 메서드 호출하여 페이징된 도서 목록을 가져옴
-        Page<Book> bookPage = bookService.findAll(pageable);
-
-        // Page에서 Content를 추출하여 List로 변환하여 모델에 추가
-        // Model에 "allBooks"라는 이름으로 페이징된 도서 목록 추가
-        model.addAttribute("allBooks", bookPage.getContent());  // bookPage.getContent() : 현재 페이지의 도서 목록
-        return "book-all";
-    }
-
     @GetMapping("/bookSearch")
     public String searchBook(Model model, @RequestParam("term") String searchTerm) {
 
